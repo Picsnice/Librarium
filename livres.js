908ef800d192723ef5be928daa39b656e71938ff
Picsnice <qgoigoux@gmail.com>
2025-03-28 00:22:09 +0100

    Update livres.js

--- livres.js
+++ livres.js
@@ -3,8 +3,8 @@ const triSelect = document.getElementById('tri');
 const btnAjout = document.getElementById('go-to-search');
 
 btnAjout.addEventListener('click', () => {
-  window.location.href = "ajouter-livre.html"
-  ; // Remplace par ta future page de recherche dédiée si besoin
+  window.location.href = "ajouter-livre.html";
+  // Remplace par ta future page de recherche dédiée si besoin
 });
 
 function chargerCollection() {
